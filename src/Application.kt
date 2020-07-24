c3fc1a522a34cee1132d980ff15b1e25b765ac2e
darshanaswath <39420536+darshanaswath@users.noreply.github.com>
2020-07-24 09:44:57 +0530

    interesting

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,22 @@
 package com.darshan.xD
 
 import io.ktor.application.*
+import io.ktor.http.ContentType
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    routing {
+       get(path = "/"){
+           call.respondText(text = "My First ktor try :)",contentType = ContentType.Text.Plain)
+       }
+    }
 }
 
